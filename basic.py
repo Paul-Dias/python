7920e17cc465b5693f9bb83c4febce1ad49a8216
-P <84252242+Paul-Dias@users.noreply.github.com>
2024-03-16 21:11:47 -0300

    first

---
+++ basic.py
@@ -0,0 +1,26 @@
+print(1+2,8,sep='-', end='Nome: ') # separadores
+print("Paulo")
+
+#descobre o tipo
+print(type("Paulo"),type(1))
+
+
+#boolean
+print(type(10==2))
+
+print(str(1)+'a')
+
+
+#resto div, padrão
+print(8%2 ==0)
+print(2**2)
+
+#printar varias vezes o texto
+nome='Paulo' * 10
+print(nome) 
+
+peso=70.0
+h=1.75
+print(peso/(1.75**2))
+
+print(f'Peso: {peso} h= {h:.1f}')
